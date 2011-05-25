e3ce0f11a0c68fa0556ffc705b7c739f4ed2ea47
Tony Arcieri <tony.arcieri@gmail.com>
2011-05-25 02:42:15 -0600

    Disable RubyGems when Sam specs are running

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,16 @@
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 FIXTURE_PATH = File.expand_path('../fixtures', __FILE__)
 
+# Pull in RSpec mocks ahead of time before we disable RubyGems
+require 'rspec/mocks'
+
+# Remove RubyGems so we're sure we're testing our stubs
+Object.send :remove_const, :Gem
+
+# Fix RubyGems' modifications to Kernel.require
+module Kernel
+  alias_method :require, :gem_original_require
+end
+
 require 'sam'
 require 'sam/manager'
