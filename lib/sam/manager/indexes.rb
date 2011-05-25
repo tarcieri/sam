2bb079bc44d2cf148fde2810cf91784b8ac7ffd5
Tony Arcieri <tony.arcieri@gmail.com>
2011-05-24 23:34:42 -0600

    Initial index setup support

--- lib/sam/manager/indexes.rb
+++ lib/sam/manager/indexes.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module Sam
   module Indexes
     DEFAULT_SOURCES = ['rubygems.org']
@@ -7,10 +9,24 @@ module Sam
     
     # Path to the index files
     def path
-      "~/.sam/indexes"
+      @@path ||= "~/.sam/indexes"
+    end
+    def path=(new_path)
+      @@path = new_path
     end
     
     # Gem sources
     def sources; DEFAULT_SOURCES; end
+    
+    # Setup the indexes
+    def setup
+      FileUtils.mkdir_p path
+      @@index_files = {}
+      
+      sources.each do |source|
+        file = "#{path}/#{source}.index"
+        @@index_files[source] = file
+      end
+    end
   end
 end
--- spec/manager/indexes_spec.rb
+++ spec/manager/indexes_spec.rb
@@ -1,13 +1,31 @@
 require 'spec_helper'
+require 'tmpdir'
+require 'fileutils'
 
 describe Sam::Indexes do
+  before do
+    @index_path = Dir.mktmpdir('sam_index_test')
+  end
+  after do
+    FileUtils.rm_rf @index_path
+  end
+  
   context :environment do
-    it "knows the path to the indexes" do
-      Sam::Indexes.path.should == "~/.sam/indexes"
+    it "configures path to indexes" do
+      Sam::Indexes.path = @index_path
+      Sam::Indexes.path.should == @index_path
     end
     
     it "knows the sources" do
       Sam::Indexes.sources.should == ["rubygems.org"]
+    end
+    
+    it "sets up the environment" do
+      Sam::Indexes.path = @index_path
+      
+      proc do
+        Sam::Indexes.setup
+      end.should_not raise_exception
     end
   end
 end
