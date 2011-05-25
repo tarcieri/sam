05498d95ebac1472a3756d49f45872364d6d1f4f
Tony Arcieri <tony.arcieri@gmail.com>
2011-05-24 20:07:27 -0600

    Specs for listing and counting available packages

--- spec/manager/package_index_spec.rb
+++ spec/manager/package_index_spec.rb
@@ -5,6 +5,24 @@ INDEX_FILE    = '/tmp/package.index'
 EXAMPLE_SPECS = FIXTURE_PATH + "/source/latest_specs.4.8"
 
 describe Sam::PackageIndex do
+  before do
+    FileUtils.rm_rf INDEX_FILE 
+    @index = Sam::PackageIndex.new INDEX_FILE
+    @index.load_specs File.read(EXAMPLE_SPECS)
+  end
+  
+  it "lists available packages" do
+    list = @index.list
+    gems = Dir[FIXTURE_PATH + "/gems/*"].map { |path| path[/\w+$/] }
+    gems.each do |gem|
+      list.should include(gem)
+    end
+  end
+  
+  it "counts available packages" do
+    @index.count.should == Dir[FIXTURE_PATH + "/gems/*"].size
+  end
+  
   context :import do
     before :each do
       FileUtils.rm_rf INDEX_FILE 
@@ -13,12 +31,12 @@ describe Sam::PackageIndex do
     
     it "imports latest_specs.4.8 files" do
       @index.load_specs File.read(EXAMPLE_SPECS)
-      @index.count.should == Dir[FIXTURE_PATH + "/gems"].size
+      @index.count.should == Dir[FIXTURE_PATH + "/gems/*"].size
     end
     
     it "imports latest_specs.4.8.gz files" do
       @index.load_specs File.read(EXAMPLE_SPECS + '.gz')
-      @index.count.should == Dir[FIXTURE_PATH + "/gems"].size
+      @index.count.should == Dir[FIXTURE_PATH + "/gems/*"].size
     end
   end
 end
