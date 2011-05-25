6b0d778a4ea876ad78f7f13e12079d0bd604103b
Tony Arcieri <tony.arcieri@gmail.com>
2011-05-24 20:01:35 -0600

    Support for loading gzipped package index files

--- lib/sam/manager/package_index.rb
+++ lib/sam/manager/package_index.rb
@@ -1,8 +1,12 @@
 require 'pstore'
+require 'zlib'
+require 'stringio'
 
 module Sam
   # Maintains an index of packages available on a given gem server
   class PackageIndex
+    GZIP_MAGIC = [31, 139] # Magic numbers for gzip files
+    
     # Create a new package index at the given path
     def initialize(filename)
       @store = PStore.new filename
@@ -10,10 +14,17 @@ module Sam
     
     # Load raw gem specification data
     def load_specs(data)
+      old_data = data
+      
+      if data.bytes.first(2) == GZIP_MAGIC
+        gz = Zlib::GzipReader.new StringIO.new(data)
+        data = gz.read
+        gz.close
+      end
+            
       # Load all specs in a single transaction
       @store.transaction do
         Marshal.load(data).each do |name, version, platform|
-          name = name.intern
           platforms = @store[name] || {}
           platforms[platform] = version.to_s
           @store[name] = platforms
--- spec/manager/package_index_spec.rb
+++ spec/manager/package_index_spec.rb
@@ -5,10 +5,20 @@ INDEX_FILE    = '/tmp/package.index'
 EXAMPLE_SPECS = FIXTURE_PATH + "/source/latest_specs.4.8"
 
 describe Sam::PackageIndex do
-  it "imports latest_specs.4.8 files" do
-    FileUtils.rm_rf INDEX_FILE 
-    index = Sam::PackageIndex.new INDEX_FILE
-    index.load_specs File.read(EXAMPLE_SPECS)
-    index.count.should == Dir[FIXTURE_PATH + "/gems"].size
+  context :import do
+    before :each do
+      FileUtils.rm_rf INDEX_FILE 
+      @index = Sam::PackageIndex.new INDEX_FILE
+    end
+    
+    it "imports latest_specs.4.8 files" do
+      @index.load_specs File.read(EXAMPLE_SPECS)
+      @index.count.should == Dir[FIXTURE_PATH + "/gems"].size
+    end
+    
+    it "imports latest_specs.4.8.gz files" do
+      @index.load_specs File.read(EXAMPLE_SPECS + '.gz')
+      @index.count.should == Dir[FIXTURE_PATH + "/gems"].size
+    end
   end
 end
